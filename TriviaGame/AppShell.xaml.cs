2fa24a938d91a4852e4b522d2addcfcf07161762
ZigareenKavumaNakintu <163073801+ZigareenKavumaNakintu@users.noreply.github.com>
2024-12-27 16:11:25 +0000

    Add project files.

---
+++ TriviaGame/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TriviaGame
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TriviaGame/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TriviaGame
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
